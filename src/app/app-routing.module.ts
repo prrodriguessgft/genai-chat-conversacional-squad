bfe79f9e2ca38bdf33ef11d6558fd0912f45f623
prrodriguess <paulo.rodrigues@ailos.coop.br>
2024-06-14 16:51:54 -0300

    serviço mocado atualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { ModuloComponent } from '../app/modulo/modulo.component';
+import { ModuloComponent } from '../app/modulo/modulo.component';
 
 const routes: Routes = [
+  { path: 'client-modules', component: ModuloComponent },
+  { path: '', redirectTo: '/client-modules', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/clientes-mocados.service.ts
+++ src/app/clientes-mocados.service.ts
@@ -1,63 +1,63 @@
-// import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-// export interface Client {
-//   name: string;
-//   email: string;
-//   admin: boolean;
-// }
+export interface Client {
+  name: string;
+  email: string;
+  admin: boolean;
+}
 
-// @Injectable({providedIn: 'root'})
-// export class ClientMockService {
-//   clients: Client[] = [
-//     {
-//       name: 'João Sistem',
-//       email: 'joao.sistem@gmail.com',
-//       admin: false
-//     },
-//     {
-//       name: 'Wakin',
-//       email: 'Wakin.sistem@gmail.com',
-//       admin: true
-//     },
-//     {
-//       name: 'Sinyo',
-//       email: 'Sinyo.sistem@gmail.com',
-//       admin: false
-//     },
-//     {
-//       name: 'Xuwo',
-//       email: 'Xuwo.sistem@gmail.com',
-//       admin: true
-//     },
-//     {
-//       name: 'Bubea',
-//       email: 'Bubea.sistem@gmail.com',
-//       admin: false
-//     },
-//     {
-//       name: 'Vesye',
-//       email: 'Vesye.sistem@gmail.com',
-//       admin: true
-//     },
-//     {
-//       name: 'Sanodimu',
-//       email: 'Sanodimu.sistem@gmail.com',
-//       admin: false
-//     },
-//     {
-//       name: 'Elryo',
-//       email: 'Elryo.sistem@gmail.com',
-//       admin: true
-//     },
-//     {
-//       name: 'Gaaer',
-//       email: 'Gaaer.sistem@gmail.com',
-//       admin: false
-//     },
-//     {
-//       name: 'Mioss',
-//       email: 'Mioss.sistem@gmail.com',
-//       admin: true
-//     },
-//   ]
-// }
+@Injectable({providedIn: 'root'})
+export class ClientMockService {
+  clients: Client[] = [
+    {
+      name: 'João Sistem',
+      email: 'joao.sistem@gmail.com',
+      admin: false
+    },
+    {
+      name: 'Wakin',
+      email: 'Wakin.sistem@gmail.com',
+      admin: true
+    },
+    {
+      name: 'Sinyo',
+      email: 'Sinyo.sistem@gmail.com',
+      admin: false
+    },
+    {
+      name: 'Xuwo',
+      email: 'Xuwo.sistem@gmail.com',
+      admin: true
+    },
+    {
+      name: 'Bubea',
+      email: 'Bubea.sistem@gmail.com',
+      admin: false
+    },
+    {
+      name: 'Vesye',
+      email: 'Vesye.sistem@gmail.com',
+      admin: true
+    },
+    {
+      name: 'Sanodimu',
+      email: 'Sanodimu.sistem@gmail.com',
+      admin: false
+    },
+    {
+      name: 'Elryo',
+      email: 'Elryo.sistem@gmail.com',
+      admin: true
+    },
+    {
+      name: 'Gaaer',
+      email: 'Gaaer.sistem@gmail.com',
+      admin: false
+    },
+    {
+      name: 'Mioss',
+      email: 'Mioss.sistem@gmail.com',
+      admin: true
+    },
+  ]
+}
